b08d2f82b40baf40a7543d0082d93780e0e3870c
RaulCarSan <racasan2002@gmail.com>
2024-11-22 13:55:08 +0100

    Fin clases

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -1,13 +1,19 @@
 package org.iesalandalus.programacion.robot.modelo;
 
 public enum Orientacion {
-    NORTE,
-    NORESTE,
-    ESTE,
-    SURESTE,
-    SUR,
-    SUROESTE,
-    OESTE,
-    NOROESTE;
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste");
 
+    private final String orientacion;
+
+    private Orientacion(String orientacion){
+        this.orientacion = orientacion;
+
+    }
 }
