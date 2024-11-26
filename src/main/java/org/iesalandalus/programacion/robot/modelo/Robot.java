ef023948823cbe020be5772dbb2be4155a4d9de2
RaulCarSan <racasan2002@gmail.com>
2024-11-26 14:40:35 +0100

    Clase terminada

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
@@ -1,10 +1,17 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import java.util.Objects;
+
 public class Robot {
+
+
 
     private Zona zona;
     private Coordenada coordenada;
     private Orientacion orientacion;
+
+
+
 
     public Robot(){
         zona = new Zona();
@@ -16,12 +23,12 @@ public class Robot {
         this.zona = zona;
     }
 
-    public Robot(Zona zona, Coordenada coordenada){
+    public Robot(Zona zona, Orientacion orientacion){
         this.zona = zona;
-        this.coordenada = coordenada;
+        this.orientacion = orientacion;
     }
 
-    public Robot(Zona zona, Coordenada coordenada, Orientacion orientacion){
+    public Robot(Zona zona, Orientacion orientacion, Coordenada coordenada){
         this.zona = zona;
         this.coordenada = coordenada;
         this.orientacion = orientacion;
@@ -41,6 +48,132 @@ public class Robot {
         return orientacion;
     }
 
+    public void setOrientacion(Orientacion orientacion) {
+        this.orientacion = orientacion;
+    }
 
+    public Coordenada getCoordenada() {
+        return coordenada;
+    }
+
+    public void setCoordenada(Coordenada coordenada) throws RobotExcepcion {
+
+        if (zona.pertenece(coordenada) == false){
+            throw new RobotExcepcion("Las coordenadas no pueden estar fuera de la zona");
+        }
+
+        this.coordenada = coordenada;
+    }
+
+    public void setZona(Zona zona) {
+        this.zona = zona;
+    }
+
+    public void avanzar()throws RobotExcepcion{
+
+        int nuevaX = coordenada.x();
+        int nuevaY = coordenada.y();
+
+        switch (orientacion){
+
+            case NORTE -> nuevaY ++;
+
+            case NORESTE -> {
+                                nuevaX++;
+                                nuevaY++;
+                            }
+
+            case ESTE -> nuevaX++;
+
+            case SURESTE -> {
+                                nuevaX++;
+                                nuevaY--;
+                            }
+
+            case SUR -> nuevaY--;
+
+            case SUROESTE -> {
+                                nuevaX--;
+                                nuevaY--;
+                                }
+
+            case OESTE -> nuevaX--;
+
+            case NOROESTE -> {
+                                nuevaX--;
+                                nuevaY++;
+                                }
+
+        }
+
+        try {
+            setCoordenada(new Coordenada(nuevaX,nuevaY));
+        } catch (RobotExcepcion e) {
+            throw new RobotExcepcion("No se puede avanzar, ya que se sale de la zona.");
+        }
+    }
+
+    public void girarALaDerecha(){
+
+        switch (orientacion){
+
+            case NORTE -> orientacion = Orientacion.NORESTE;
+
+            case NORESTE -> orientacion = Orientacion.ESTE;
+
+            case ESTE -> orientacion = Orientacion.SURESTE;
+
+            case SURESTE -> orientacion = Orientacion.SUR;
+
+            case SUR -> orientacion = Orientacion.SUROESTE;
+
+            case SUROESTE -> orientacion = Orientacion.OESTE;
+
+            case OESTE -> orientacion = Orientacion.NOROESTE;
+
+            case NOROESTE -> orientacion = Orientacion.NORTE;
+        }
+
+    }
+
+
+    public void girarALaIzquierda(){
+        switch (orientacion){
+
+            case NORTE -> orientacion = Orientacion.NOROESTE;
+
+            case NOROESTE -> orientacion = Orientacion.OESTE;
+
+            case OESTE -> orientacion = Orientacion.SUROESTE;
+
+            case SUROESTE -> orientacion = Orientacion.SUR;
+
+            case SUR -> orientacion = Orientacion.SUROESTE;
+
+            case SURESTE -> orientacion = Orientacion.ESTE;
+
+            case ESTE -> orientacion = Orientacion.NORESTE;
+
+            case NORESTE -> orientacion = Orientacion.NORTE;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Robot robot = (Robot) o;
+        return Objects.equals(zona, robot.zona) && Objects.equals(coordenada, robot.coordenada) && orientacion == robot.orientacion;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(zona, coordenada, orientacion);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[zona=%s, coordenada=%s, orientacion=%s]", zona, coordenada, orientacion);
+    }
 
 }
